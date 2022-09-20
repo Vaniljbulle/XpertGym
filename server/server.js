fda2a7d5131eba005056274c92f812c289e82689
Vaniljbulle <vinxad@gmail.com>
2022-09-20 17:35:30 +0200

    Changes by Martin

--- server/server.js
+++ server/server.js
@@ -8,13 +8,13 @@ app.use(express.json());
 const indexPaths = ['/', '/index', '/home', '/index.html'];
 app.get(indexPaths, async (req, res) => {
     console.log("Index page requested");
-    res.sendFile('index.html', {root : path.join('frontend')});
+    res.sendFile('index.html', {root: path.join('frontend')});
 })
 
 // Style sheet
 app.get('/style.css', async (req, res) => {
     console.log("Style sheet requested");
-    res.sendFile('style.css', {root : path.join('frontend')});
+    res.sendFile('style.css', {root: path.join('frontend')});
 })
 
 // Login page
@@ -34,11 +34,11 @@ app.get(registerPaths, async (req, res) => {
 // Register script (client side)
 app.get('/register.js', async (req, res) => {
     console.log("Register script requested");
-    res.sendFile('register.js', {root : path.join('server')});
+    res.sendFile('register.js', {root: path.join('server')});
 })
 
 // Register post request
-app.post('/api/register', async(req, res) => {
+app.post('/api/register', async (req, res) => {
     console.log("Register post request");
     console.log(req.body);
     // register user
