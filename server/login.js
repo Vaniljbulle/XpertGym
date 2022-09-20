840cbd9f1507d75b16cd7add65cf77105026e958
Tempron <wickgrenm@gmail.com>
2022-09-21 00:52:27 +0200

    Added login.js

---
+++ server/login.js
@@ -0,0 +1,28 @@
+const form = document.getElementById('login_form')
+form.addEventListener('submit', login)
+
+async function login(event) {
+    event.preventDefault()
+    const username = document.getElementById('username').value
+    const password = document.getElementById('password').value
+
+    const result = await fetch('/api/login', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            username,
+            password
+        })
+    }).then((res) => res.json())
+
+    if (result.status === 'ok') {
+        // everything went fine
+        console.log('Got the token: ', result.data)
+        localStorage.setItem('token', result.data)
+        alert('Success')
+    } else {
+        alert(result.error)
+    }
+}
