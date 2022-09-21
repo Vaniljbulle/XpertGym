376d0321a183285cf20b3b44db0a8b0c5d1ddbb0
Tempron <wickgrenm@gmail.com>
2022-09-21 18:15:05 +0200

    Fixed the login logic

--- server/server.js
+++ server/server.js
@@ -112,23 +112,23 @@ app.post('/api/login', async (req, res) => {
     const user = await User.findOne({ username }).lean()
     console.log(user);
 
-    // if (!user) {
-    //     return res.json({ status: 'error', error: 'Invalid username/password' })
-    // }
-    //
-    // if (await bcrypt.compare(entered.password, user.password)) {
-    //     // the username, password combination is successful
-    //
-    //     const token = jwt.sign(
-    //         {
-    //             id: user._id,
-    //             username: user.username
-    //         },
-    //         JWT_SECRET
-    //     )
-    //
-    //     return res.json({ status: 'ok', data: token })
-    // }
+    if (!user) {
+        return res.json({ status: 'error', error: 'Invalid username/password' })
+    }
+
+    if (await bcrypt.compare(password, user.password)) {
+        // the username, password combination is successful
+
+        const token = jwt.sign(
+            {
+                id: user._id,
+                username: user.username
+            },
+            JWT_SECRET
+        )
+
+        return res.json({ status: 'ok', data: token })
+    }
 
     res.json({ status: 'error', error: 'Invalid username/password' })
 })
