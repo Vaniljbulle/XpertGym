74c7f374ec24d405023914bf2c537d69335a5ac6
FREEDOM <vinxad@gmail.com>
2022-10-05 17:33:31 +0200

    unit test for getting index.html

--- unit tests/api_html.test.js
+++ unit tests/api_html.test.js
@@ -7,20 +7,43 @@ const app = express();
 app.use(express.json());
 app.use(htmlRoutes);
 
+function indexExpect(response) {
+    let expected;
+    try {
+        expected = fs.readFileSync('./frontend/index.html', 'utf8');
+    } catch (err) {
+        console.error(err);
+    }
+
+    expect(response.type).toBe('text/html');
+    expect(response.text).toBe(expected);
+    expect(response.statusCode).toBe(200);
+}
+
 describe('api_html', () => {
-    let expected = fs.readFileSync('./frontend/index.html', 'utf8');
+    describe('index.html', () => {
+        test('responds to /', async () => {
+            indexExpect(await request(app).get('/'));
+        });
 
-    test('responds to /', async () => {
-        const response = await request(app).get('/');
+        test('responds to /index', async () => {
+            indexExpect(await request(app).get('/index'))
+        });
 
-        expect(response.text).toBe(expected);
-        expect(response.statusCode).toBe(200);
-    });
+        test('responds to /index.html', async () => {
+            indexExpect(await request(app).get('/index.html'))
+        });
 
-    test('responds to /index', async () => {
-        const response = await request(app).get('/index');
+        test('responds to /home', async () => {
+            indexExpect(await request(app).get('/home'))
+        });
 
-        expect(response.text).toBe(expected);
-        expect(response.statusCode).toBe(200);
+        test('responds to /home.html', async () => {
+            indexExpect(await request(app).get('/home.html'))
+        });
+
+        test('responds to /iNdEx', async () => {
+            indexExpect(await request(app).get('/iNdEx'))
+        });
     });
 });
