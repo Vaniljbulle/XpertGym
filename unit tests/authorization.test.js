f303547f57b80b33159e51141662e6876027b726
FREEDOM <vinxad@gmail.com>
2022-10-06 18:34:28 +0200

    renamed file, tests focusing on authentication

---
+++ unit tests/authorization.test.js
@@ -0,0 +1,110 @@
+request = require('supertest');
+const express = require('express');
+const loginRoute = require('../server/api/api_login.js');
+const registerRoute = require('../server/api/api_register.js');
+const logoutRoute = require('../server/api/api_logout.js');
+const mongoose = require('mongoose');
+const User = require("../server/database/user");
+
+const app = express();
+app.use(express.json());
+app.use(loginRoute);
+app.use(registerRoute);
+app.use(logoutRoute);
+
+beforeAll(async () => await mongoose.connect('mongodb://127.0.0.1:27017/xpertdb', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}));
+
+describe('api_register.js', function () {
+    /*
+     *   Create a test user
+     *   Login with test user
+     *   Test token validation
+     *   Logout
+     *   Delete test user
+     */
+
+    const testUser = {username: "testUserRegistration", password: "testPassword", id_number: "48946548974"};
+    describe('POST /api/register', () => {
+        const falseUsers = [
+            {username: "", password: "testPassword", id_number: "48946548974"},
+            {username: "wetwetw", password: "", id_number: "48946548974"},
+            {username: "awbrwnrqateant", password: "testPassword", id_number: ""},
+            {username: "", password: "", id_number: ""},
+            {username: "", password: "", id_number: "48946548974"},
+            {username: "", password: "testPassword", id_number: ""},
+            {username: "qe5tketqmetg", password: "", id_number: ""},
+        ];
+
+        test('Proper user info', async () => {
+            const response = await request(app).post('/api/register').set('Content-Type', "application/json").send(JSON.stringify(testUser));
+            expect(response.statusCode).toBe(200);
+            expect(response.body.status).toBe('ok')
+        });
+
+        test('Username taken', async () => {
+            const response = await request(app).post('/api/register').set('Content-Type', "application/json").send(JSON.stringify(testUser));
+            expect(response.statusCode).toBe(500);
+            expect(response.body.status).toBe('error');
+        });
+
+        // For each false user, test if the response is 400 and the status is error
+        falseUsers.forEach((falseUser, index) => {
+            test(`False user ${index}`, async () => {
+                const response = await request(app).post('/api/register').set('Content-Type', "application/json").send(JSON.stringify(falseUser));
+                expect(response.statusCode).toBe(400);
+                expect(response.body.status).toBe('error');
+            });
+        });
+
+
+    });
+
+    describe('POST /api/login', function () {
+        test('Wrong password', async () => {
+            const response = await request(app).post('/api/login').set('Content-Type', "application/json").send(JSON.stringify({
+                username: testUser.username,
+                password: "wrongPasswordlelelelle"
+            }));
+
+            expect(response.body.status).toBe('error');
+        })
+
+        test('No user', async () => {
+            const response = await request(app).post('/api/login').set('Content-Type', "application/json").send(JSON.stringify({
+                username: "Idontexist",
+                password: "wrongPasswordlelelelle"
+            }));
+
+            expect(response.body.status).toBe('error');
+        })
+
+        test('Login (token request)', async () => {
+            const res = await request(app).post('/api/login').set('Content-Type', "application/json").send(JSON.stringify({
+                username: testUser.username,
+                password: testUser.password
+            }));
+
+            expect(res.body.header).toBe('Authorization');
+            expect(res.body.token).not.toBe(null);
+            expect(res.body.status).toBe('ok');
+        });
+    });
+
+});
+
+afterAll(async () => {
+    /*
+        const users = await User.find({});
+        users.forEach(user => {
+            console.log(user.username);
+        });
+    */
+
+    await User.deleteMany({});
+
+
+    await mongoose.connection.close();
+});
