bbd16e43dcfd6ef3936b18133ef2117c5e0338ba
Tempron <wickgrenm@gmail.com>
2022-10-07 16:09:39 +0200

    Added database format for schedule

---
+++ server/database/schedule.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+
+const scheduleSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    id_user: {
+        type: String,
+        required: true
+    },
+    id_number: {
+        type: String,
+        required: true,
+        unique: true
+    }
+},{collection: 'schedules'});
+
+const model = mongoose.model('ScheduleSchema', scheduleSchema);
+
+module.exports = model;
