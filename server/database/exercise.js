6fe9f0d89a055ef5c1814abd4cffc0e2706706b3
Tempron <wickgrenm@gmail.com>
2022-10-07 16:09:26 +0200

    Added database format for exercise

---
+++ server/database/exercise.js
@@ -0,0 +1,45 @@
+const mongoose = require('mongoose');
+
+const exerciseSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    image: {
+        type: String,
+        required: true
+    },
+    sets: {
+        type: Number,
+        required: true
+    },
+    reps: {
+        type: Number,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    muscleGroup: {
+        type: Number,
+        required: true
+    },
+    duration: {
+        type: Number,
+        required: true
+    },
+    difficulty: {
+        type: Number,
+        required: true
+    },
+    id_number: {
+        type: String,
+        required: true,
+        unique: true
+    }
+},{collection: 'exercises'});
+
+const model = mongoose.model('ExerciseSchema', exerciseSchema);
+
+module.exports = model;
