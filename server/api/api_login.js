9ee4501584ecc654754004658a0a7398b17b0cc6
FREEDOM <vinxad@gmail.com>
2022-10-10 16:24:23 +0200

    refactor token sign, generate refresh token, return accessToken/refreshToken

--- server/api/api_login.js
+++ server/api/api_login.js
@@ -1,37 +1,34 @@
 const bcrypt = require("bcrypt");
 const User = require("../database/user");
 const router = require('express').Router();
-const jwt = require('jsonwebtoken');
-
-const JWT_SECRET = 'sdjkfh8923yhjdksbfma@#*(&@*!^#&@bhjb2qiuhesdbhjdsfg839ujkdhfjk'
+const {generateAccessToken, generateRefreshToken} = require('../auth/token');
 
 // Login post request
 router.post('/api/login', async (req, res) => {
     console.log(req.body);
     const {username, password} = req.body
-    const user = await User.findOne({ username }).lean()
+    const user = await User.findOne({username}).lean()
     console.log(user);
 
     if (!user) {
-        return res.json({ status: 'error', error: 'Invalid username/password' })
+        return res.json({status: 'error', error: 'Invalid username/password'})
     }
 
     if (await bcrypt.compare(password, user.password)) {
         // the username, password combination is successful
 
-        const token = jwt.sign(
-            {
-                id: user._id,
-                username: user.username
-            },
-            JWT_SECRET,
-            { expiresIn: '1h' }
-        )
+        const tokenUser = {id: user._id, username: user.username};
+        const accessToken = generateAccessToken(tokenUser);
+        const refreshToken = generateRefreshToken(tokenUser);
 
-        return res.json({ header: 'Authorization', status: 'ok', data: token })
+        return res.json({
+            header: 'Token',
+            status: 'ok',
+            data: {accessToken: accessToken, refreshToken: refreshToken}
+        })
     }
 
-    res.json({ status: 'error', error: 'Invalid username/password' })
+    res.json({status: 'error', error: 'Invalid username/password'})
 })
 
 module.exports = router
