7f252bb4a6b46474e294e75ee2066aa026c7ae31
FREEDOM <vinxad@gmail.com>
2022-10-10 19:15:08 +0200

    Added new token route to server

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const registerRoute = require('./server/api/api_register.js');
 const loginRoute = require('./server/api/api_login.js');
 const htmlRoutes = require('./server/api/api_html.js');
 const logoutRoute = require('./server/api/api_logout.js');
+const tokenRoute = require('./server/api/api_token.js');
 
 const app = express();
 
@@ -15,6 +16,7 @@ app.use(loginRoute);
 app.use(htmlRoutes);
 app.use('/img', express.static('img'));
 app.use(logoutRoute);
+app.use(tokenRoute);
 
 
 mongoose.connection.on('disconnected', function() {
