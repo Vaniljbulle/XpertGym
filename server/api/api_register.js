5fcfb3182dca9071c7e55b24c3b1d1b209bb289a
Tempron <43298787+Tempron@users.noreply.github.com>
2022-10-11 19:12:26 +0200

    Removed commented code and added membership to the register api

--- server/api/api_register.js
+++ server/api/api_register.js
@@ -1,5 +1,6 @@
 const bcrypt = require("bcrypt");
 const User = require("../database/user");
+const Membership = require("../database/membership");
 const router = require('express').Router();
 
 // Register post request
@@ -7,29 +8,18 @@ router.post('/api/register', async (req, res) => {
     console.log("Register post request");
 
     // Check input for validity
-    if (req.body.username === undefined || req.body.password === undefined || req.body.id_number === undefined) {
+    if (req.body.username === undefined || req.body.password === undefined || req.body.membership_id === undefined) {
         res.status(400).json({status: 'error', data: 'Invalid input'});
         return;
     }
 
-    //console.log(req.body);
-    const {username, password, id_number} = req.body;
+    const {username, password, membership_id} = req.body;
     const hashedPassword = await bcrypt.hash(password, 10);
-    //console.log(hashedPassword);
-    /*
-        await User.create({username, password: hashedPassword, id_number}, (err, user) => {
-            if (err) {
-                console.log(err);
-                res.status(500).json({status: 'error'});
-            } else {
-                console.log(user);
-                res.status(200).json({status: 'ok', data: user});
-            }
-        })
-    */
 
     try {
-        const user = await User.create({username, password: hashedPassword, id_number});
+        const membership = await Membership.findOne({membership_id: membership_id}).lean();
+        const user = await User.create({username, password: hashedPassword});
+        membership.update({user_id: user._id});
         console.log("User created successfully: ", user);
         res.status(200).json({status: 'ok', data: username});
     }
@@ -37,9 +27,6 @@ router.post('/api/register', async (req, res) => {
         console.log("Encountered error during sign up: ", err);
         res.status(500).json({status: 'error'});
     }
-    //res.json({status: 'ok', data: 'Data test'});
-
-
 })
 
 module.exports = router;
