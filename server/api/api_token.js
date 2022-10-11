62f19bb4fd563b5666bb65cb06c3934853592d8d
Vaniljbulle <vinxad@gmail.com>
2022-10-11 17:51:09 +0200

    changed response, removed refreshing refresh token

--- server/api/api_token.js
+++ server/api/api_token.js
@@ -1,19 +1,16 @@
 const router = require('express').Router();
-const {verifyRefreshToken} = require('../auth/tokenValidation');
-const {generateAccessToken, generateRefreshToken} = require("../auth/token");
+const {verifyRefreshToken, generateAccessToken, verifyAccessToken} = require("../auth/token");
 
 // Refresh token post request
 router.get('/api/token', verifyRefreshToken, async (req, res) => {
     console.log("Refresh token post request");
-    console.log(req.headers);
+    //console.log(req.headers);
 
     const tokenUser = {id: req.user.id, username: req.user.username};
     const accessToken = generateAccessToken(tokenUser);
-    const refreshToken = generateRefreshToken(tokenUser);
 
     res.cookie('accessToken', accessToken, {httpOnly: true});
-    res.cookie('refreshToken', refreshToken, {httpOnly: true});
-    res.redirect('back');
+    res.status(200).json({status: 'ok', data: 'Refreshed token'});
 })
 
 module.exports = router
