2ee198e2c77075215978d2789006d905712d9e37
Vaniljbulle <vinxad@gmail.com>
2022-10-11 17:51:38 +0200

    Clear tokens from database on logout

--- server/api/api_logout.js
+++ server/api/api_logout.js
@@ -1,9 +1,12 @@
 const router = require('express').Router();
-const {verifyAccessToken} = require('../auth/tokenValidation');
+const {verifyAccessToken} = require('../auth/token');
+const mongoose = require('mongoose');
+const User = require('../database/user');
 
 // Logout post request
 router.post('/api/logout', verifyAccessToken, async (req, res) => {
     console.log("Logout post request");
+    await User.findOneAndUpdate({username: req.user.username}, {refreshTokens: []});
 
     res.clearCookie('accessToken');
     res.clearCookie('refreshToken');
