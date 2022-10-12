4305e8d2540e5bab923ee4ebbcb75f2610042361
Tempron <wickgrenm@gmail.com>
2022-10-12 20:07:53 +0200

    Fixed api_register.js, so it works with memberships, you can also not use an existing username anymore

--- server/api/api_register.js
+++ server/api/api_register.js
@@ -14,16 +14,25 @@ router.post('/api/register', async (req, res) => {
     }
 
     const {username, password, membership_id} = req.body;
+
+    if (await User.findOne({username}).lean()) {
+        res.status(403).json({status: 'error', data: 'Username already exists'});
+        return;
+    }
+
     const hashedPassword = await bcrypt.hash(password, 10);
 
     try {
         const membership = await Membership.findOne({membership_id: membership_id}).lean();
+        if (!membership) throw "No membership found";
+
         const user = await User.create({username, password: hashedPassword});
-        membership.update({user_id: user._id});
+
+        await Membership.updateOne({membership_id: membership_id}, {$set: {user_id: user._id}});
+
         console.log("User created successfully: ", user);
         res.status(200).json({status: 'ok', data: username});
-    }
-    catch (err) {
+    } catch (err) {
         console.log("Encountered error during sign up: ", err);
         res.status(500).json({status: 'error'});
     }
