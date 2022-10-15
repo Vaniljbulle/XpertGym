d679411f0657894ced733c181e001a13370b1be0
FREEDOM <vinxad@gmail.com>
2022-10-15 22:16:31 +0200

    So many words for something thats EMPTY

--- server/auth/gym_pop_client.js
+++ server/auth/gym_pop_client.js
@@ -3,7 +3,7 @@ const population_data = document.getElementById("population_data_ID");
 fetch('/api/gympopulationdata')
     .then(response => response.json())
     .then(data => {
-        if (isNaN(data.currentPop) || data.currentPop === undefined) {
+        if (isNaN(data.currentPop) || data.currentPop === undefined || data.currentPop === null) {
             population_data.innerHTML = "";
         } else {
             population_data.innerHTML = "Gym population: " + data.currentPop + "%";
