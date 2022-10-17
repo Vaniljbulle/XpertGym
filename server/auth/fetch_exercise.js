a1eaab4c60325c52e71dddaee34bcab58c8ee24d
Tempron <wickgrenm@gmail.com>
2022-10-17 14:19:02 +0200

    Changed fetch_exercise.js so it returns instead of spawning html

--- server/auth/fetch_exercise.js
+++ server/auth/fetch_exercise.js
@@ -1,7 +1,4 @@
 function fetchAll() {
-
-    let exercises = [];
-
     fetch('/api/exercise/getAll', {
         method: 'POST',
         headers: {
@@ -11,16 +8,14 @@ function fetchAll() {
     }).then(res => res.json())
         .then(res => {
             if (res.status === 'ok') {
-                console.log('All exercises fetched successfully!');
-                exercises = JSON.parse(JSON.stringify(res.data));
-                console.log(exercises);
-                for (let i = 0; i < exercises.length; i++) {
-                    document.getElementById('cardHolder').innerHTML += "<p>" + exercises[i].name + "</p>";
-                    document.getElementById('cardHolder').innerHTML += "<img" + " src='/img/" + exercises[i].image + "'>";
-
-                }
+                return res.data;
+                // for (let i = 0; i < exercises.length; i++) {
+                //     document.getElementById('cardHolder').innerHTML += "<p>" + exercises[i].name + "</p>";
+                //     document.getElementById('cardHolder').innerHTML += "<img" + " src='/img/" + exercises[i].image + "'>";
+                //
+                // }
             } else {
-                alert('All exercises failed to be fetched!');
+                console.log('All exercises failed to be fetched!');
             }
         }).catch(err => console.log(err));
 
