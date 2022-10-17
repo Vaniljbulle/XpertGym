ac757415b71ee7add8ec4af4ac08e17b682f57b9
Tempron <wickgrenm@gmail.com>
2022-10-17 14:16:29 +0200

    Added user checking in api_exercise.js

--- server/api/api_exercise.js
+++ server/api/api_exercise.js
@@ -1,8 +1,13 @@
 const Exercise = require("../database/exercise");
+const UserExercise = require("../database/userExercise");
+const Schedule = require("../database/schedule");
+const {verify} = require("jsonwebtoken");
+const {verifyToken} = require("../auth/token");
+const isAdmin = require("../auth/isAdmin");
 const router = require('express').Router();
 
 // Add exercise post request
-router.post('/api/exercise/add', async (req, res) => {
+router.post('/api/exercise/add', verifyToken, async (req, res) => {
     console.log("Exercise add post request");
 
     // Check input for validity
@@ -14,26 +19,35 @@ router.post('/api/exercise/add', async (req, res) => {
         req.body.muscleGroup === undefined ||
         req.body.duration === undefined ||
         req.body.difficulty === undefined ||
-        req.body.custom === undefined) {
+        req.body.type === undefined) {
         res.status(400).json({status: 'error', data: 'Invalid input'});
         return;
     }
 
-    const {name, image, sets, reps, description, muscleGroup, duration, difficulty, custom} = req.body;
+    const {name, image, sets, reps, description, muscleGroup, duration, difficulty, type} = req.body;
 
     try {
-        const user = await Exercise.create({name, image, sets, reps, description, muscleGroup, duration, difficulty, custom});
+        const user = await Exercise.create({
+            name,
+            image,
+            sets,
+            reps,
+            description,
+            muscleGroup,
+            duration,
+            difficulty,
+            type
+        });
         console.log("Exercise added successfully: ", user);
         res.status(200).json({status: 'ok', data: name});
-    }
-    catch (err) {
+    } catch (err) {
         console.log("Encountered error during adding exercise: ", err);
         res.status(500).json({status: 'error'});
     }
 })
 
 // Remove exercise post request
-router.post('/api/exercise/remove', async (req, res) => {
+router.post('/api/exercise/remove', verifyToken, async (req, res) => {
     console.log("Exercise remove post request");
 
     const filter = {};
@@ -53,23 +67,40 @@ router.post('/api/exercise/remove', async (req, res) => {
         const user = await Exercise.findById(_id).deleteOne();
         console.log("Exercise removed successfully: ", user);
         res.status(200).json({status: 'ok'});
-    }
-    catch (err) {
+    } catch (err) {
         console.log("Encountered error during exercise removal: ", err);
         res.status(500).json({status: 'error'});
     }
 })
 
 // All exercises get request
-router.post('/api/exercise/getAll', async (req, res) => {
+router.post('/api/exercise/all', verifyToken, async (req, res) => {
     console.log("Send all get request");
 
-    const filter = {};
-    const all = await Exercise.find(filter);
+    if (await isAdmin(req.user)) {
+        try {
+            const filter = {};
+            const all = await Exercise.find(filter);
 
-    console.log(all);
+            console.log("All exercises was sent");
+            res.status(200).json({status: 'ok', data: all});
+        } catch (err) {
+            res.status(500).json({status: 'error', data: err});
+        }
+    } else {
+        try {
+            let all = await Exercise.find({type: 0}).lean();
+            const customExercises = await UserExercise.find({id_user: req.user.id}).lean();
+            for (const customExercise of customExercises) {
+                all += await Exercise.findById(customExercise.id_exercise).lean();
+            }
 
-    res.status(200).json({status: 'ok', data: all});
+            console.log("All allowed exercises was sent");
+            res.status(200).json({status: 'ok', data: all});
+        } catch (err) {
+            res.status(500).json({status: 'error', data: err});
+        }
+    }
 })
 
 module.exports = router;
