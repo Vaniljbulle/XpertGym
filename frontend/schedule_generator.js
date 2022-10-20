60c8a305f45b2eba6142cb047a70f1f0d1c1a102
Tempron <43298787+Tempron@users.noreply.github.com>
2022-10-20 12:39:36 +0200

    Added the ability to fetch all and spawn text in cards

--- frontend/schedule_generator.js
+++ frontend/schedule_generator.js
@@ -2,6 +2,8 @@ const fill = document.querySelector('.planner-card-fill');
 const empties = document.querySelectorAll('.planner-card-empty');
 const exerciseModal = document.getElementById("planner-modal-container");
 
+const categories = document.getElementsByClassName("workout-container");
+
 const name = document.getElementById("name_value");
 const sets = document.getElementById("sets_value");
 const reps = document.getElementById("reps_value");
@@ -11,10 +13,64 @@ const duration = document.getElementById("duration_value");
 const muscle = document.getElementsByName("muscleGroup");
 const diff = document.getElementsByName("difficulty");
 
-let exercises = fetchAll();
+let sortedExercises = new Array(6);
+for (let i = 0; i < sortedExercises.length; i++) {
+    sortedExercises[i] = new Array(0);
+}
+let exercises = [];
+loadAllExercises();
 
-for (const exercise of exercises) {
-    console.log(exercise);
+function loadAllExercises() {
+    fetch('/api/exercise/all', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({})
+    }).then(res => res.json())
+        .then(res => {
+            if (res.status === 'ok') {
+                console.log(res.data);
+                console.log("Fetched");
+                for (let i = 0; i < res.data.length; i++) {
+                    console.log(res.data[i]);
+                    fetchExercise(res.data[i]);
+                }
+            } else {
+                console.log('All exercises failed to be fetched!');
+            }
+        }).catch(err => console.log(err));
+}
+
+function getExerciseClass(exercise) {
+    switch (exercise.muscleGroup) {
+        case 0:
+            return "chest-card-empty";
+        case 1:
+            return "arms-card-empty";
+        case 2:
+            return "legs-card-empty";
+        case 3:
+            return "back-card-empty";
+        case 4:
+            return "abs-card-empty";
+        case 5:
+            return "cardio-card-empty";
+        default:
+            return "";
+    }
+}
+
+function spawnExerciseCard(exercise) {
+    const category = getExerciseClass(exercise);
+    if (category === "") return;
+    let card = document.createElement("div");
+    let text = document.createElement("p");
+    text.innerHTML = exercise.name;
+    card.className = category;
+    card.appendChild(text);
+    categories[exercise.muscleGroup].appendChild(card);
+
 }
 
 function exitCreateExercise() {
@@ -51,7 +107,7 @@ function createExercise() {
     console.log(_muscleGroup);
     console.log(_difficulty);
 
-    addExercise({
+    const data = {
         name: name.value,
         image: "/img/wlogos.png",
         sets: sets.value,
@@ -61,14 +117,58 @@ function createExercise() {
         duration: duration.value,
         difficulty: _difficulty,
         type: 0
-    })
-    //     .then(data => {
-    //     if (data !== null) {
-    //         console.log(data);
-    //         exitCreateExercise();
-    //     } else
-    //         alert("Failed to add exercise!");
-    // })
+    }
+
+    fetch('/api/exercise/add', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    }).then(res => res.json())
+        .then(res => {
+            console.log(res);
+            if (res.status === 'ok') {
+                console.log(res.data);
+                exitCreateExercise();
+                fetchExercise(res.data);
+            } else {
+                alert("Failed to add exercise!");
+            }
+        }).catch(err => console.log(err));
+
+}
+
+function fetchExercise(id) {
+    fetch('/api/exercise/getByID', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({_id: id})
+    }).then(res => res.json())
+        .then(res => {
+            if (res.status === 'ok') {
+                console.log("An exercise was fetched");
+                const data = {
+                    name: res.data.name,
+                    image: res.data.image,
+                    sets: res.data.sets,
+                    reps: res.data.reps,
+                    description: res.data.description,
+                    muscleGroup: res.data.muscleGroup,
+                    duration: res.data.duration,
+                    difficulty: res.data.difficulty,
+                    type: res.data.type
+                };
+                sortedExercises[res.data.muscleGroup].push(data);
+                const index = sortedExercises[res.data.muscleGroup].length - 1;
+                const element = sortedExercises[res.data.muscleGroup][index];
+                spawnExerciseCard(element);
+            } else {
+                console.log('Exercise failed to be fetched!');
+            }
+        }).catch(err => console.log(err));
 }
 
 // Fill listeners
