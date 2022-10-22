70a4f83e3d3d42b61af185fcb1e9144184e6df94
GkevinG <guvenalkevin@gmail.com>
2022-10-22 21:12:02 +0200

    add a few push exercises

--- server/database/generateDefaultExercises.js
+++ server/database/generateDefaultExercises.js
@@ -5,6 +5,8 @@ async function getAllExercises() {
 
     // If there are no exercises, generate them
     if (exercises.length === 0) {
+        //region Chest exercises
+        /***** Chest exercises *****/
         generateDefaultExercise(
             "Bench Press",
             "img/icons/bench-press.png",
@@ -12,10 +14,102 @@ async function getAllExercises() {
             10,
             "Lay down on a bench and lift the barbell up and down",
             0,
-            60,
+            0,
+            2,
+            2
+        );
+        generateDefaultExercise(
+            "Incline Bench Press",
+            "img/icons/bench-press.png",
+            4,
+            10,
+            "Lay down on a bench with a incline between 30° to 45° degrees and lift the barbell up and down",
+            0,
+            0,
+            2,
+            2
+        );
+        generateDefaultExercise(
+            "Decline Bench Press",
+            "img/icons/bench-press.png",
+            4,
+            10,
+            "Lay down on a bench with a decline around -20° to -30° degrees and lift the barbell up and down",
+            0,
+            0,
+            2,
+            2
+        );
+        generateDefaultExercise(
+            "Machine Chest Press",
+            "img/icons/bench-press.png",
+            4,
+            10,
+            "Sit down in the machine and use your chest muscles to press the handles away from your body moving them out until your arms are extended and return slowly.",
+            0,
+            0,
             1,
             2
         );
+        generateDefaultExercise(
+            "Push-up",
+            "img/icons/pushups.png",
+            4,
+            10,
+            "Get down on all fours and straighten your arms and legs, lower your body until your chest nearly touches the floor. Pause, then push yourself back up. If the exercise is too heavy you can use your knees as support.",
+            0,
+            0,
+            1,
+            2
+        );
+        generateDefaultExercise(
+            "Dip",
+            "img/icons/bench-press.png",
+            4,
+            10,
+            "Push through your chest extending your arms and stopping just before locking out. Focus on using your chest since this exercise can be used for triceps also.",
+            0,
+            0,
+            1,
+            2
+        );
+        generateDefaultExercise(
+            "Dumbbell pull-over",
+            "img/icons/bench-press.png",
+            3,
+            10,
+            "Lay down on a bench and hold the dumbbell with both your hands on the part furthest away from you. Slowly let it fall behind your head then push it up to bench press position.",
+            0,
+            0,
+            3,
+            2
+        );
+        generateDefaultExercise(
+            "Machine fly",
+            "img/icons/bench-press.png",
+            4,
+            10,
+            "Have your arms aligned with your chest on the sides extended and grip the handles. ",
+            0,
+            0,
+            1,
+            2
+        );
+        //endregion
+        //region Tricep exercises
+        /***** Tricep exercises *****/
+        generateDefaultExercise(
+            "Dip",
+            "img/icons/bench-press.png",
+            4,
+            10,
+            "Push through your chest extending your arms and stopping just before locking out. Focus on using your triceps (arms) since this exercise can be used for chest also.",
+            1,
+            0,
+            1,
+            2
+        );
+        //endregion
     }
 }
 
