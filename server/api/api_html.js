0d9e5d56f0a0b5a506c7ca2240cfaa8e208023ef
FREEDOM <vinxad@gmail.com>
2022-10-24 22:48:56 +0200

    conflict fix

--- server/api/api_html.js
+++ server/api/api_html.js
@@ -9,11 +9,11 @@ const isAdmin = require('../auth/isAdmin');
 // Get profile.html
 router.get('/profile.html', verifyToken, async (req, res) => {
     res.sendFile('profile.html', {root: 'frontend'});
+});
 
 // GET message_board.js
 router.get('/message_board.js', async (req, res) => {
     res.sendFile('message_board.js', {root: 'server/auth'});
-
 });
 
 // GET devMessaging.js
